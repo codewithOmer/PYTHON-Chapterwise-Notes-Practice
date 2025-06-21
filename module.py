2620989280da7a0160f46e7f1b19d0389bd96a55
codewithOmer <mdomer1881@gmail.com>
2025-06-21 16:47:29 +0530

    CHAPTER 1 -Modules and comments

---
+++ module.py
@@ -0,0 +1,15 @@
+import pyjokes
+
+print("printing jokes...")
+
+# this prints a random jokes   [to commet out [CTRL + /] which ignores the line]
+joke = pyjokes.get_joke()
+print(joke)
+
+""" hi my name is  [for multiple line coment out [we use triple [" or ']]
+mohammed omer
+my age is 18""" 
+
+# hi my name is 
+# mohammed omer  [0r select all the line and coment out[by CTRL + /]]
+# my age is 18   [it get sigle line coment out]
